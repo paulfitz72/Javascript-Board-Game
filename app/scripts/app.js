68387c404a2c3f011fc3b14acdc3614992248ea7
paulfitz72 <paulf88@hotmail.com>
2015-07-08 15:48:33 +0100

    First commit generated template from yeoman

---
+++ app/scripts/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name twentyfourtyeightApp
+ * @description
+ * # twentyfourtyeightApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('twentyfourtyeightApp', [
+    'ngCookies'
+  ]);
